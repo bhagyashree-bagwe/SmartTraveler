ab47293bb240b81ab57b273c802d64ac5ae9c8e6
shubhamkumbhalkar <34781200+shubhamkumbhalkar@users.noreply.github.com>
2018-11-29 23:14:39 -0600

    Agent Functionality Done
    Add update delete user. Added Trending Link

--- src/Utilities.java
+++ src/Utilities.java
@@ -45,11 +45,13 @@ public class Utilities extends HttpServlet{
 					result = result + "<li><a href='Logout'><span> Logout</span></a></li>"
 							+ " <li><a href='Account'><span> Account</span></a></li>"
 							+ " <li><a href='ViewHotels'> View Hotels </a></li>"
+							+ " <li><a href='Trending'> Trending </a></li>"
 							+ " <li><a><i>Hi,"+name+"</i></a></li>";
 				}
 				else if(usertype().equalsIgnoreCase("agent")){
 					result = result + "<li><a href='Logout'><span> Logout</span></a></li>"
 									+ " <li><a href='ManageCustomer'>Manage Customers</a></li>"
+									+ " <li><a href='Trending'> Trending </a></li>"
 									+ " <li><a><i>Hi,"+name+" (Agent)</i></a></li>";
 
 				}
@@ -59,6 +61,7 @@ public class Utilities extends HttpServlet{
 							+ " <li><a href='ViewHotels'> View Hotels </a></li>"
 							+ " <li><a href='DataExploration'> Data Exploration </a></li>"
 							+ "<li><a href='Admin'><span> Modify Hotel Info</span></a></li>"
+							+ " <li><a href='Trending'> Trending </a></li>"
 							+ " <li><a><i>Hi,"+name+" (Admin)</i></a></li>";
 				}
 			}
